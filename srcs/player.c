f46f474efc02cb9d25b6bfb2c720f25a226ee8f8
Horemheb89 <horemheb89@yahoo.co.uk>
2026-07-21 09:22:26 +0200

    raycasting, textures, movements, player

---
+++ srcs/player.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: catrenet <catrenet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/17 15:17:51 by catrenet          #+#    #+#             */
+/*   Updated: 2026/07/18 13:58:40 by catrenet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+static void	init_north(t_data *d)
+{
+	d->dir_x = 0;
+	d->dir_y = -1;
+	d->plane_x = 0.66;
+	d->plane_y = 0;
+}
+
+static void	init_south(t_data *d)
+{
+	d->dir_x = 0;
+	d->dir_y = 1;
+	d->plane_x = -0.66;
+	d->plane_y = 0;
+}
+
+static void	init_west(t_data *d)
+{
+	d->dir_x = -1;
+	d->dir_y = 0;
+	d->plane_x = 0;
+	d->plane_y = -0.66;
+}
+
+static void	init_east(t_data *d)
+{
+	d->dir_x = 1;
+	d->dir_y = 0;
+	d->plane_x = 0;
+	d->plane_y = 0.66;
+}
+
+/* +0.5 to put player in middle of cell */
+void	init_player(t_data *d)
+{
+	d->pos_x = d->player_x + 0.5;
+	d->pos_y = d->player_y + 0.5;
+	if (d->player_dir == 'N')
+		init_north(d);
+	else if (d->player_dir == 'S')
+		init_south(d);
+	else if (d->player_dir == 'E')
+		init_east(d);
+	else if (d->player_dir == 'W')
+		init_west(d);
+}
---
+++ srcs/texture.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   texture.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: catrenet <catrenet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/17 11:28:57 by catrenet          #+#    #+#             */
+/*   Updated: 2026/07/21 09:15:27 by catrenet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+// Load one XPM texture file into image struct
+static void	load_one_texture(t_data *d, t_img *tex, char *path)
+{
+	tex->ptr = mlx_xpm_file_to_image(d->mlx, path, &tex->width, &tex->height);
+	if (!tex->ptr)
+		error_exit(d, "Failed to load texture");
+	tex->addr = mlx_get_data_addr(tex->ptr, &tex->bpp, &tex->line_len,
+			&tex->endian);
+	if (!tex->addr)
+		error_exit(d, "Failed to get texture data");
+}
+
+void	load_textures(t_data *d)
+{
+	load_one_texture(d, &d->tex[TEX_NO], d->cfg.no);
+	load_one_texture(d, &d->tex[TEX_SO], d->cfg.so);
+	load_one_texture(d, &d->tex[TEX_WE], d->cfg.we);
+	load_one_texture(d, &d->tex[TEX_EA], d->cfg.ea);
+}
+
+// Pick correct textures to apply based on side + direction
+static void	pick_texture(t_ray *ray)
+{
+	if (ray->side == 0)
+	{
+		if (ray->dir_x > 0)
+			ray->texture_id = TEX_EA;
+		else
+			ray->texture_id = TEX_WE;
+	}
+	else
+	{
+		if (ray->dir_y > 0)
+			ray->texture_id = TEX_SO;
+		else
+			ray->texture_id = TEX_NO;
+	}
+}
+
+/* set spot on wall where ray hits (0.0 to 1.0) to pick column of texture */
+void	tex_and_wallx(t_data *d, t_ray *ray)
+{
+	if (ray->side == 0)
+		ray->wall_tex_x = d->pos_y + ray->perp_dist * ray->dir_y;
+	else
+		ray->wall_tex_x = d->pos_x + ray->perp_dist * ray->dir_x;
+	ray->wall_tex_x -= floor(ray->wall_tex_x);
+	pick_texture(ray);
+}
+
+int	tex_color(t_data *d, t_ray *ray, int tex_x, int tex_y)
+{
+	t_img	*tex;
+	char	*pixel;
+
+	tex = &d->tex[ray->texture_id];
+	pixel = tex->addr + (tex_y * tex->line_len + tex_x * (tex->bpp / 8));
+	return (*(unsigned int *)pixel);
+}
