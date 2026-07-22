d485a626952037dc2950427d810aa1c11e5df309
Ziakimbogo <83517906+Ziakimbogo@users.noreply.github.com>
2026-07-22 19:09:49 +0200

    yo

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: gechavia <gechavia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/11 16:28:56 by gechavia          #+#    #+#             */
-/*   Updated: 2026/07/22 19:07:39 by gechavia         ###   ########.fr       */
+/*   Updated: 2026/07/22 19:09:42 by gechavia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,8 +117,7 @@ typedef struct s_data
 	double		dir_y;
 	double		plane_x;
 	double		plane_y;
-	
-	
+	t_img		tex[4];
 }	t_data;
 
 typedef struct s_ray
